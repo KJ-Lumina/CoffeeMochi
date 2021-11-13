7dd303aef33ca3d8c76200407f92eec11c7e2f22
Mochida011 <liaw.k@digipen.edu>
2021-11-14 00:37:09 +0800

    change population.h to struct

--- population.h
+++ population.h
@@ -4,162 +4,293 @@
 //#include <Resource_stats.h>
 
 
+#define COMMON 0 
+#define SECOND 1
+#define FIRST 2
+
 #define STUDENT 0
 #define ELDERLY 1
 #define KID 2
 
-int i;
-int c;
-int s;
-int a;
-int g;
-int current_morale = 50;
-int current_food = 50;
-int current_gold = 50;
+#define OFFICEWORKER 0 
+#define RETAILOWNER 1
+#define TEACHER 2
+
+#define DOCTOR 0
+#define MINISTER 1
+#define LAWYER 2
 
 
-//struct citizen {
-//
-//    int morale;
-//    int food;
-//    int gold;
-//
-//};
-//struct common_citizen {
-//
-//};
-//struct second_tier {
-//
-//};
-//struct first_tier {
-//
-//};
+#define SAD 0 //morale is low 
+#define HUNGRY 1 //low on food
+#define BROKE 2 //no money 
+#define HAPPY 3 //too high on morale
+#define FULL 4 // too much food
+#define RICH 5 // too much gold
 
-//void common_citizen_s() {
-//    //https://overiq.com/c-programming-101/array-of-strings-in-c/
-//
-//}
-void common_citizen() {
-    c = (rand() % 4);
-    g = (rand() % 2);
-    //char ans, Y='y', N='n';
-    char common[4][30] = { "student", "elderly", "kid", "cleaner" };
+int tier
 
-    if (c == STUDENT) {
-        switch (g)
-        {
-        case 0:
-            //crops decrease by 5
-            current_food = current_food - 5;
-            //morale increase by 5
-            current_morale = current_morale + 5;
-            //finance increase by 5
-            current_gold = current_gold + 5;
-            printf("I am an immigrant and would like to stay in your country to study\n");
-            printf("%d,%d,%d", current_food, current_gold, current_morale);
+
+
+
+struct PROFILE{
+    char name[];
+    //char descrip[];
+    int age;
+    int needs;
+    char occupation[];
+
+};
+
+PROFILE create_citizen(){
+int second_gen;
+int first_gen;
+int tier_gen= (rand() % 20);
+    if (tier_gen >= 0 && tier_gen< 10) {
+        tier = COMMON;
+        int common_gen = (rand() % 3);
+        int name_gen = (rand() % 5);
+        
+        switch (common_gen) {
+            
+        case STUDENT: //student
+            //{ "student", "elderly", "kid" };
+            occupation = "student";
+            age = (rand() % 10) + 13;
+            if (name_gen == 0) {
+                name ="student john";
+            }
+            else if (name_gen == 1) {
+                name ="student karen";
+            }
+            else if (name_gen == 2) {
+                name ="student bob";
+            }
+            else if (name_gen == 3) {
+                name ="student nancy";
+            }
+            else if (name_gen == 4) {
+                name ="student george";
+            }
+            else if (name_gen == 5) {
+                name ="student travess";
+            }
+
             break;
 
-        case 1:
-            //crops decrease by 5
-            current_food = current_food - 5;
-            //morale increase by 10
-            current_morale = current_morale + 10;
-            //finance decrease by 5
-            current_gold = current_gold - 5;
+        case ELDERLY:
+            occupation = "senior citizen";
+            age = (rand() % 10) + 60;
+            int name_gen = (rand() % 5);
 
-            printf("I need an education and i come on a scholarship\n");
+            if (name_gen == 0) {
+                name = "elderly john";
+            }
+            else if (name_gen == 1) {
+                name = "elderly karen";
+            }
+            else if (name_gen == 2) {
+                name = "elderly bob";
+            }
+            else if (name_gen == 3) {
+                name = "elderly nancy";
+            }
+            else if (name_gen == 4) {
+                name = "elderly george";
+            }
+            else if (name_gen == 5) {
+                name = "elderly travess";
+            }
             break;
+
+        case KID:
+            occupation = "kid";
+            age = (rand() % 8) +5;
+            if (name_gen == 0) {
+                name = "kid john";
+            }
+            else if (name_gen == 1) {
+                name = "kid karen";
+            }
+            else if (name_gen == 2) {
+                name = "kid bob";
+            }
+            else if (name_gen == 3) {
+                name = "kid nancy";
+            }
+            else if (name_gen == 4) {
+                name = "kid george";
+            }
+            else if (name_gen == 5) {
+                name = "kid travess";
+            }
+
+            break;
+
         }
-
+        
     }
-    else if (c == ELDERLY) {
-        switch (g)
-        {
-        case 0:
-            //crops decrease by 3
-            current_food = current_food - 5;
-            //morale increase by 5
-            current_morale = current_morale + 10;
-            //finance supported
-
-            printf("I am seeking shelter from the storm \n");
+    else if (tier_gen >= 10 && tier_gen < 17) {
+        tier = SECOND;
+        int name_gen = (rand() % 5);
+        second_gen = rand() % 3;
+        switch (second_gen) {
+        case OFFICEWORKER://0
+            occupation = "office worker";
+            age = (rand() % 8) + 30;
+            if (name_gen == 0) {
+                name = "colleague john";
+            }
+            else if (name_gen == 1) {
+                name = "colleague karen";
+            }
+            else if (name_gen == 2) {
+                name = "colleague bob";
+            }
+            else if (name_gen == 3) {
+                name = "colleague nancy";
+            }
+            else if (name_gen == 4) {
+                name = "colleague george";
+            }
+            else if (name_gen == 5) {
+                name = "colleague travess";
+            }
             break;
 
-        case 1:
 
-            printf("my family is inside please let me in\n");
-            //if ((scanf_s("y or n:%c\n", ans))==Y) {
-                //crops decrease by 5
-            current_food = current_food - 5;
-            //morale increase by 10
-            current_morale = current_morale + 10;
-            //finance decrease by 5
-            current_gold = current_gold - 5;
-            printf("changes been made");
-            //}
+        case RETAILOWNER://1
+            age = (rand() % 8) + 25;
+            occupation = "retail owner";
+            int name_gen = (rand() % 5);
+            if (name_gen == 0) {
+                name = "boss john";
+            }
+            else if (name_gen == 1) {
+                name = "boss karen";
+            }
+            else if (name_gen == 2) {
+                name = "boss bob";
+            }
+            else if (name_gen == 3) {
+                name = "boss nancy";
+            }
+            else if (name_gen == 4) {
+                name = "boss george";
+            }
+            else if (name_gen == 5) {
+                name = "boss travess";
+            }
             break;
+
+        case TEACHER://2
+            occupation = "teacher";
+            age = (rand() % 8) + 20;
+            if (name_gen == 0) {
+                name = "teacher john";
+            }
+            else if (name_gen == 1) {
+                name = "teacher karen";
+            }
+            else if (name_gen == 2) {
+                name = "teacher bob";
+            }
+            else if (name_gen == 3) {
+                name = "teacher nancy";
+            }
+            else if (name_gen == 4) {
+                name = "teacher george";
+            }
+            else if (name_gen == 5) {
+                name = "teacher travess";
+            }
+            break;
+
         }
-    }
-    printf("%s", common[c]);
-    //common citizen common stats
-    //farmer boost crops boost money
-    //chef boost happiness reduce crops increase money 
-    // use random int to cycle between characters if create citizen lands on common citizen 
-
-}
-void special_citizen() {
-    s = (rand() % 4);
-    char special[4][60] = { "doctor", "chef", "farmer", "banker" };
-    //citizens like doctor etc
-    //use random int to cycle between characters if create citizen lands on special citizen 
-    //maybe put array in array 
-
-    printf("%s", special[s]);
-}
-void authorized_citizen() {
-    a = (rand() % 4);
-    char authorized[4][60] = { "advisor", "soldier", "governor", "detective" };
-    printf("%s", authorized[a]);
-    //lawyer, army, detective
-    //use random int to cycle between characters if create citizen lands on authorized citizen
-}
-void create_citizen() {
-    //3 kinds citizen array
-    //55% for common citizen
-    //30% for second tier
-    //15% for first tier
-    int ir;
-    ir = (rand() % 20);
-    if (ir >= 0 && ir <= 10) {
-        i = 0;
-    }
-    else if (ir > 10 && ir <= 16) {
-        i = 1;
-    }
-    else if (ir > 16 && ir < 20) {
-        i = 2;
-    }
-    const char citizen[3][20] = { "civilian", "second tier", "first tier" };
-    //--------------------------------------------------------------------
-    printf("%s\n", &citizen[i]);
-
-    //switch case for types of citizen
-    if (i == 0) {
-        common_citizen();
-    }
-    else if (i == 1) {
-        special_citizen();
     }
     else {
-        authorized_citizen();
-    }
-    //-------------------------------------------------------------------
-    //set into new variables into array 
+        tier = FIRST;
+        occupation = "doctor";
+        first_gen = rand() % 3;
+        switch (second_gen) {
+        case DOCTOR://0
+            age = (rand() % 8) + 20;
+            if (name_gen == 0) {
+                name = "teacher john";
+            }
+            else if (name_gen == 1) {
+                name = "teacher karen";
+            }
+            else if (name_gen == 2) {
+                name = "teacher bob";
+            }
+            else if (name_gen == 3) {
+                name = "teacher nancy";
+            }
+            else if (name_gen == 4) {
+                name = "teacher george";
+            }
+            else if (name_gen == 5) {
+                name = "teacher travess";
+            }
+            break;
 
+
+        case MINISTER://1
+            occupation = "minister";
+            age = (rand() % 8) + 20;
+            if (name_gen == 0) {
+                name = "teacher john";
+            }
+            else if (name_gen == 1) {
+                name = "teacher karen";
+            }
+            else if (name_gen == 2) {
+                name = "teacher bob";
+            }
+            else if (name_gen == 3) {
+                name = "teacher nancy";
+            }
+            else if (name_gen == 4) {
+                name = "teacher george";
+            }
+            else if (name_gen == 5) {
+                name = "teacher travess";
+            }
+            break;
+
+
+        case LAWYER://2
+            occupation = "lawyer";
+            age = (rand() % 8) + 20;
+            if (name_gen == 0) {
+                name = "lawyer john";
+            }
+            else if (name_gen == 1) {
+                name = "lawyer karen";
+            }
+            else if (name_gen == 2) {
+                name = "lawyer bob";
+            }
+            else if (name_gen == 3) {
+                name = "lawyer nancy";
+            }
+            else if (name_gen == 4) {
+                name = "lawyer george";
+            }
+            else if (name_gen == 5) {
+                name = "lawyer travess";
+            }
+            break;
+
+        }
+    }
+    
+    return PROFILE{ name, age , occupation };
 }
-void citizen() {
-    //calls create citizen, runs and display chosen citizen 
-}
+
+
+
 void game_init(void)
 {
 
