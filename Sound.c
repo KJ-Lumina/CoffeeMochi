c689e901b78a516215741b3e8e14258cb90d8e97
Travesstck <travesstanck@gmail.com>
2021-11-27 11:34:10 +0800

    sounds bug fix :/

--- Sound.c
+++ Sound.c
@@ -14,24 +14,33 @@
 #define RAT 6
 
 
+
 typedef struct {
     int soundIndex;
     int soundState;
     int soundLocation;
 }
 SOUND;
+
+CP_Sound CardDeck_S;
+CP_Sound Click_S;
+CP_Sound Gold_S;
+CP_Sound House_S;
+CP_Sound Market_S;
+CP_Sound RatE_S;
+CP_Sound Tavern_S;
+CP_Sound Crops_S;
 //PlayingSound[20];
 void Init()
 {
-    
-    CardDeck_S = CP_Sound_Load(".\Sounds\CardSFX.wav3");
-    Click_S = CP_Sound_Load(".\Sounds\ClickSFX.wav3");
-    Gold_S = CP_Sound_Load(".\Sounds\GoldSFX.wav3");
-    House_S = CP_Sound_Load(".\Sounds\HouseSFX.wav3");
-    Market_S = CP_Sound_Load(".\Sounds\MarketSFX.wav3");
-    RatE_S = CP_Sound_Load(".\Sounds\RatSFX.wav3");
-    Tavern_S = CP_Sound_Load(".\Sounds\TavernSFX.wav3");
-    Crops_S = CP_Sound_Load(".\Sounds\HouseSFX.wav3");
+    CardDeck_S = CP_Sound_Load("./Sounds/CardSFX.wav3");
+    Click_S = CP_Sound_Load("./Sounds/ClickSFX.wav3");
+    Gold_S = CP_Sound_Load("./Sounds/GoldSFX.wav3");
+    House_S = CP_Sound_Load("./Sounds/HouseSFX.wav3");
+    Market_S = CP_Sound_Load("./Sounds/MarketSFX.wav3");
+    RatE_S = CP_Sound_Load("./Sounds/RatSFX.wav3");
+    Tavern_S = CP_Sound_Load("./Sounds/TavernSFX.wav3");
+    Crops_S = CP_Sound_Load("./Sounds/HouseSFX.wav3");
 }
 
 void PlaySound(int SoundIndex)
@@ -39,76 +48,64 @@ void PlaySound(int SoundIndex)
     if (CP_Input_MouseTriggered(MOUSE_BUTTON_1)) 
     {
         CP_Sound_Play(Click_S);
-        CP_Sound_Free(Click_S);
+        CP_Sound_Free(&Click_S);
     }
     return;
-    switch (soundIndex) 
+    switch (SoundIndex)
     {
     case CARD:
         CP_Sound_Play(CardDeck_S);
-        SOUND.soundState = PLAY;
         break;
     
     case HOUSE:
         CP_Sound_Play(House_S);
-        SOUND.soundState = PLAY;
         break;
 
     case MARKET:
-        CP_Sound_Play(Marker_S);
-        SOUND.soundState = PLAY;
+        CP_Sound_Play(Market_S);
         break;
 
     case FARM:
-        CP_Sound_Play(Farm_S);
-        SOUND.soundState = PLAY;
+        //CP_Sound_Play(Farm_S);
         break;
 
     case GOLD:
         CP_Sound_Play(Gold_S);
-        SOUND.soundState = PLAY;
         break;
 
     case RAT:
         CP_Sound_Play(RatE_S);
-        SOUND.soundState = PLAY;
         break;
     }
     
 }
 
-void Shutdown()
+void Shutdown(int SoundIndex)
 {
-    switch (soundIndex)
+    switch (SoundIndex)
     {
     case CARD:
         CP_Sound_Free(&CardDeck_S);
-        SOUND.soundState = STOP;
         break;
 
     case HOUSE:
         CP_Sound_Free(&House_S);
-        SOUND.soundState = STOP;
         break;
 
     case MARKET:
-        CP_Sound_Free(&Marker_S);
-        SOUND.soundState = STOP;
+        CP_Sound_Free(&Market_S);
         break;
 
     case FARM:
-        CP_Sound_Free(&Farm_S);
-        SOUND.soundState = STOP;
+        //CP_Sound_Free(&Farm_S);
         break;
 
     case GOLD:
         CP_Sound_Free(&Gold_S);
-        SOUND.soundState = STOP;
         break;
 
     case RAT:
         CP_Sound_Play(RatE_S);
-        SOUND.soundState = STOP;
         break;
     }
 
