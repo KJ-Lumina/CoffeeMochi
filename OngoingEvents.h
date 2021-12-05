79ccd8dc59a45151e00406e0bc7e16c334f3044e
KJ-Lumina <90664472+KJ-Lumina@users.noreply.github.com>
2021-12-05 14:03:51 +0800

    Finish Comments Header Files

--- OngoingEvents.h
+++ OngoingEvents.h
@@ -1,3 +1,16 @@
+/*!_____________________________________________________________________________
+@file       OngoingEvents.h
+@author     Kelly Liaw (liaw.k@digipen.edu)
+@co-authors Travess Tan (travesscheekhang.t@digipen.edu)
+@course     CSD1120
+@section    B
+@team       CoffeeMochi
+@brief      This file contains the prototype function definitions that are used in
+			the ongoing events in Impero.
+
+			Copyright ©2021 DigiPen, All rights reserved.
+*//*__________________________________________________________________________*/
+
 #include <stdio.h>
 #include "cprocessing.h"
 
@@ -11,8 +24,6 @@ typedef struct {
 	int alive;
 	char description[100];
 }ONGOING;
-
-
 
 
 void RemoveEvent(int index);//check if  turn left is 0, if 0, remove event from array 
